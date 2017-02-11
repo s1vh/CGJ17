9def623b1f502b44afddba5b6db30b71965d7669
s1vh <al285854@uji.es>
2017-02-11 02:32:56 +0100

    Init repo

---
+++ Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacter : MonoBehaviour {
+
+
+    public float speed = 3f;
+    Vector3 movement;
+    Rigidbody2D playerRigidbody;
+
+
+    void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Move(h, v);
+
+        //Turning();
+
+       // Animating(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+
+        movement.Set(h, v, 0f);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CGJ17
+
+No pintas nada aqui
+
 Castellón Game Jam 2017 (o cómo llegar tarde a la GGJ por culpa de los exámenes) ;')
